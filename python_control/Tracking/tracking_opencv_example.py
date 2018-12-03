86c2238ceaadbbba30c4e001e0d6b0c67c401b44
Nico Albers <nico.albers@aboutyou.de>
2018-12-03 08:19:24 +0100

    fix and beautify tracking files, make parameterident pep8 compatible

--- python_control/Tracking/tracking_opencv_example.py
+++ python_control/Tracking/tracking_opencv_example.py
@@ -1,16 +1,29 @@
+#! /usr/bin/env python
+"""Tracking Tutorial.
+
+took and changed from
+https://www.learnopencv.com/object-tracking-using-opencv-cpp-python/
+"""
 import cv2
 import sys
 import numpy as np
 
 (major_ver, minor_ver, subminor_ver) = (cv2.__version__).split('.')
+# video_path = "/home/tim/Downloads/IMG_3161.MOV"
+# video_path = "/Users/nico.albers/OneDrive/Auto_Gruppe/Tracking_Auto/IMG_3161.MOV"
+video_path = "/Users/nico.albers/OneDrive/Auto_Gruppe/Tracking_Auto/410_308.avi"
+video_path = "/Users/nico.albers/OneDrive/Auto_Gruppe/Tracking_Auto/1280_32.avi"
+
 
 if __name__ == '__main__':
     # Set up tracker.
     # Instead of MIL, you can also use
 
-    tracker_types = ['BOOSTING', 'MIL', 'KCF', 'TLD', 'MEDIANFLOW', 'GOTURN', 'MOSSE', 'CSRT']
-    #4 oder 7
-    tracker_type = tracker_types[4]
+    tracker_types = [
+        'BOOSTING', 'MIL', 'KCF', 'TLD',
+        'MEDIANFLOW', 'GOTURN', 'MOSSE', 'CSRT']
+    # 4 oder 7 (medianflow / CSRT)
+    tracker_type = tracker_types[0]
     print (minor_ver)
     if int(minor_ver) < 3:
         tracker = cv2.Tracker_create(tracker_type)
@@ -33,7 +46,7 @@ if __name__ == '__main__':
             tracker = cv2.TrackerCSRT_create()
 
     # Read video
-    video = cv2.VideoCapture("/home/tim/Downloads/IMG_3161.MOV",cv2.IMREAD_GRAYSCALE)
+    video = cv2.VideoCapture(video_path, cv2.IMREAD_GRAYSCALE)
     # Exit if video not opened.
     if not video.isOpened():
         print ("Could not open video")
@@ -41,11 +54,11 @@ if __name__ == '__main__':
 
     # Read first frame.
     ok, frame = video.read()
-    frame = cv2.resize(frame, (0, 0), fx=0.1, fy=0.1)
-    #img = frame
-    #imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    #imgray = cv2.GaussianBlur(imgray, (5, 5), 1)
-    #frame = cv2.Canny(imgray, 50, 150)
+    # frame = cv2.resize(frame, (0, 0), fx=0.1, fy=0.1)
+    # img = frame
+    # imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # imgray = cv2.GaussianBlur(imgray, (5, 5), 1)
+    # frame = cv2.Canny(imgray, 50, 150)
     if not ok:
         print ('Cannot read video file')
         sys.exit()
@@ -62,32 +75,32 @@ if __name__ == '__main__':
     while True:
         # Read a new frame
         ok, frame = video.read()
-        frame = cv2.resize(frame, (0, 0), fx=0.1, fy=0.1)
-        #img = frame
-        #mgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        #imgray = cv2.GaussianBlur(imgray, (5, 5), 1)
-        #frame = cv2.Canny(imgray, 100, 150)
-        #frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        # img =frame
-        frame = cv2.GaussianBlur(frame, (11, 11), 0)
-        frame = hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        # frame = cv2.resize(frame, (0, 0), fx=0.1, fy=0.1)
+        # # img = frame
+        # # mgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        # # imgray = cv2.GaussianBlur(imgray, (5, 5), 1)
+        # # frame = cv2.Canny(imgray, 100, 150)
+        # # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # # img = frame
+        # frame = cv2.GaussianBlur(frame, (11, 11), 0)
+        # frame = hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-        # construct a mask for the color "green", then perform
-        # a series of dilations and erosions to remove any small
-        # blobs left in the mask
-        lower_red = np.array([0, 50, 50])
-        upper_red = np.array([10, 255, 255])
-        mask0 = cv2.inRange(frame, lower_red, upper_red)
+        # # construct a mask for the color "green", then perform
+        # # a series of dilations and erosions to remove any small
+        # # blobs left in the mask
+        # lower_red = np.array([0, 50, 50])
+        # upper_red = np.array([10, 255, 255])
+        # mask0 = cv2.inRange(frame, lower_red, upper_red)
 
-        # upper mask (170-180)
-        lower_red = np.array([170, 50, 50])
-        upper_red = np.array([180, 255, 255])
-        mask1 = cv2.inRange(frame, lower_red, upper_red)
-        mask = mask0 + mask1
+        # # upper mask (170-180)
+        # lower_red = np.array([170, 50, 50])
+        # upper_red = np.array([180, 255, 255])
+        # mask1 = cv2.inRange(frame, lower_red, upper_red)
+        # mask = mask0 + mask1
 
-        mask = cv2.erode(mask, None, iterations=2)
-        mask = cv2.dilate(mask, None, iterations=2)
-        frame = mask
+        # mask = cv2.erode(mask, None, iterations=2)
+        # mask = cv2.dilate(mask, None, iterations=2)
+        # frame = mask
 
         if not ok:
             break
@@ -98,7 +111,7 @@ if __name__ == '__main__':
         ok, bbox = tracker.update(frame)
 
         # Calculate Frames per second (FPS)
-        fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer);
+        fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer)
 
         # Draw bounding box
         if ok:
@@ -106,20 +119,27 @@ if __name__ == '__main__':
             p1 = (int(bbox[0]), int(bbox[1]))
             p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
             cv2.rectangle(frame, p1, p2, (255, 0, 0), 2, 1)
-            #print(p2[0]-p1[0],p2[1]-p1[1])
+            # print(p2[0]-p1[0],p2[1]-p1[1])
         else:
             # Tracking failure
-            cv2.putText(frame, "Tracking failure detected", (100, 80), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2)
+            cv2.putText(
+                frame, "Tracking failure detected",
+                (100, 80), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2)
 
         # Display tracker type on frame
-        cv2.putText(frame, tracker_type + " Tracker", (100, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50, 170, 50), 2);
+        cv2.putText(
+            frame, tracker_type + " Tracker",
+            (100, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50, 170, 50), 2)
 
         # Display FPS on frame
-        cv2.putText(frame, "FPS : " + str(int(fps)), (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50, 170, 50), 2);
+        cv2.putText(
+            frame, "FPS : " + str(int(fps)),
+            (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50, 170, 50), 2)
 
         # Display result
         cv2.imshow("Tracking", frame)
 
         # Exit if ESC pressed
         k = cv2.waitKey(1) & 0xff
-        if k == 27: break
+        if k == 27:
+            break
