86c2d7e13329d6529f29ea4f3b06d0595d473561
Zarbokk <timhansen93@googlemail.com>
2019-01-02 16:46:02 +0100

    update augmentation

--- python_control/projectWork/training_NN/Data_Augmentation_throw_together.py
+++ python_control/projectWork/training_NN/Data_Augmentation_throw_together.py
@@ -4,7 +4,7 @@ import time
 from os import listdir
 from os.path import isfile, join
 
-mypath="train_data/augmented_data"
+mypath="train_data/augmented_data/forward/"
 onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 first=True
 i=0
@@ -37,7 +37,7 @@ for file_name in onlyfiles:
     with open('/home/tim/Documents/Car_Pi_MUM/python_control/projectWork/training_NN/train_data/augmented_data/complete_for_training/augmented_data_FULL.csv', 'a') as f:
         aug_data = np.asarray(np.loadtxt(
             open(
-                "/home/tim/Documents/Car_Pi_MUM/python_control/projectWork/training_NN/train_data/augmented_data/" + file_name,
+                "/home/tim/Documents/Car_Pi_MUM/python_control/projectWork/training_NN/" + mypath + file_name,
                 "rb"),
             delimiter=","))
     #print(aug_data.shape)
--- python_control/projectWork/training_NN/train_week_different_models.py
+++ python_control/projectWork/training_NN/train_week_different_models.py
@@ -4,28 +4,23 @@ import numpy as np
 import time
 import cv2
 
-steps_per_epoche=10
-number_epochs=1
+steps_per_epoche = 4158
+number_epochs = 5
 
-kernel_size_first = 3
-conv_first = 16
-kernel_size_second = 2
-conv_second = 32
-kernel_size_third = 3
-conv_third = 64
-kernel_size_fourth = 3
-conv_fourth = 128
+n_kernels = (16, 32)
+kernel_size = (3, 4, 5)
+number_fc = (1, 2)
+number_neurons = (64, 128)
+learning_rates = (0.001, 0.002, 0.0005)
+batch_size = 128
 
-
-learning_rate=0.001
-batch_size = 64
 
 def generate_arrays_from_file(path, printing):
     while 1:
         f = open(path)
         i = 0
         img_rows, img_cols = 36, 64
-        x_train = np.zeros((batch_size, img_rows, img_cols,1))
+        x_train = np.zeros((batch_size, img_rows, img_cols, 1))
         y_train = np.zeros(batch_size)
         for line in f:
             myarray = np.fromstring(line, dtype=float, sep=',')
@@ -39,7 +34,7 @@ def generate_arrays_from_file(path, printing):
             # the data, split between train and test sets
             # (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
 
-            x_train[i, :, :] = x_train_tmp.reshape(1, img_rows, img_cols,1)
+            x_train[i, :, :] = x_train_tmp.reshape(1, img_rows, img_cols, 1)
             y_train[i] = y_train_tmp
             if printing:
                 print(y_train)
@@ -47,7 +42,7 @@ def generate_arrays_from_file(path, printing):
             i = i + 1
 
             if i == batch_size:
-                print(i,x_train.shape)
+                print(i, x_train.shape)
                 x_train = x_train.astype('float32')
                 y_train = y_train.astype('float32')
                 yield (x_train, y_train)
@@ -57,47 +52,41 @@ def generate_arrays_from_file(path, printing):
 
 
 img_rows, img_cols = 36, 64
-input_shape = (img_rows, img_cols,1)
+input_shape = (img_rows, img_cols, 1)
 # convert class vectors to binary class matrices
 # y_train = keras.utils.to_categorical(y_train, 1)
 # y_test = keras.utils.to_categorical(y_test, 1)
 # time.sleep(10)
-model = keras.Sequential()
-model.add(keras.layers.Conv2D(16, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
-model.add(keras.layers.MaxPooling2D(pool_size=(2, 2)))
-model.add(keras.layers.Conv2D(32, (3, 3), activation='relu'))
-model.add(keras.layers.MaxPooling2D(pool_size=(2, 2)))
-# model.add(keras.layers.Dropout(0.25))
-model.add(keras.layers.Flatten())
-# model.add(keras.layers.Dense(128, activation='relu'))
-# model.add(keras.layers.Dropout(0.5))
-model.add(keras.layers.Dense(64, activation='relu'))
-model.add(keras.layers.Dropout(0.5))
-model.add(keras.layers.Dense(1, activation='linear'))
 
-model.compile(loss=keras.losses.mean_squared_error,
-              optimizer=keras.optimizers.Adam(lr=learning_rate))
+for i in range(0, 2):
+    for j in range(0, 3):
+        for k in range(0, 2):
+            for l in range(0, 2):
+                for m in range(0, 3):
 
-model.fit_generator(generate_arrays_from_file(
-    "/home/tim/Documents/Car_Pi_MUM/python_control/projectWork/training_NN/train_data/augmented_data/complete_for_training/augmented_data_FULL.csv",
-    False),
-                    steps_per_epoch=steps_per_epoche, epochs=number_epochs)
-# model.fit_generator(generate_arrays_from_file("F:/OneDrive/Uni/StudienArbeit/Car_Dataset/augmented_data_FULL.csv",),
-#                    steps_per_epoch=10000, epochs=100)
+                    model = keras.Sequential()
+                    model.add(keras.layers.Conv2D(n_kernels[i], kernel_size=(kernel_size[j], kernel_size[j]),
+                                                  activation='relu', input_shape=input_shape))
+                    model.add(keras.layers.MaxPooling2D(pool_size=(2, 2)))
+                    model.add(
+                        keras.layers.Conv2D(n_kernels[i] * 2, (kernel_size[j], kernel_size[j]), activation='relu'))
+                    model.add(keras.layers.MaxPooling2D(pool_size=(2, 2)))
+                    model.add(keras.layers.Flatten())
+                    model.add(keras.layers.Dense(number_neurons[l], activation='relu'))
+                    model.add(keras.layers.Dropout(0.5))
+                    if number_fc[k] == 2:
+                        model.add(keras.layers.Dense(number_neurons[l], activation='relu'))
+                        model.add(keras.layers.Dropout(0.5))
+                    model.add(keras.layers.Dense(1, activation='linear'))
 
-# model.fit(x_train, y_train,
-#          batch_size=batch_size,
-#          epochs=epochs,
-#          verbose=1,
-#          shuffle=True,
-#          validation_data=(x_test, y_test))
+                    model.compile(loss=keras.losses.mean_squared_error,
+                                  optimizer=keras.optimizers.Adam(lr=learning_rates[m]))
 
-# score = model.evaluate(x_test, y_test, verbose=0)
-# print('Test loss:', score)
-#print(model.predict_generator(generate_arrays_from_file(
-#    "/home/tim/Documents/Car_Pi_MUM/python_control/projectWork/training_NN/train_data/augmented_data/complete_for_training/augmented_data_FULL.csv",
-#    True), 10, max_queue_size=1))
-# print(y_train)
-
-#model.save("/home/tim/Documents/Car_Pi_MUM/python_control/projectWork/training_NN/models/test_model_complete_64x36.h5")
-# model.save_weights("/home/tim/Documents/Car_Pi_MUM/python_control/projectWork/training_NN/models/test_model_64x36.h5")
+                    model.fit_generator(generate_arrays_from_file(
+                        "/home/tim/Documents/Car_Pi_MUM/python_control/projectWork/training_NN/train_data/augmented_data/complete_for_training/augmented_data_FULL.csv",
+                        False),
+                        steps_per_epoch=steps_per_epoche, epochs=number_epochs)
+                    model.save(
+                        "/home/tim/Documents/Car_Pi_MUM/python_control/projectWork/training_NN/models/week_models_20_{0}_{1}_{2}_{3}_{4}.h5".format(
+                            i, j, k, l, m))
+                    print(i, j, k, l, m,)
